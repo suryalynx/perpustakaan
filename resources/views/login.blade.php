b949ea40709fb396eaa3836fcf1b00552cc0e391
suryalynx <arantasurya455@gmail.com>
2024-02-13 17:47:46 +0800

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -22,7 +22,8 @@
                     <div class="card shadow-lg">
                         <div class="card-body p-5">
                             <h1 class="fs-4 card-title fw-bold mb-4">Login</h1>
-                            <form method="POST" class="needs-validation" novalidate="" autocomplete="off">
+                            <form class="needs-validation" method="post" action="{{route('login')}}">
+                                    @csrf
                                 <div class="mb-3">
                                     <label class="mb-2 text-muted" for="email">Email/Username</label>
                                     <input id="login" type="text" class="form-control" name="login"
